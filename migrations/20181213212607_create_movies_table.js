a1eb462e7e789fb7145c855bbd81de1b5487f181
Arthur <arthuroliveirabandeira@gmail.com>
2019-02-26 23:18:45 -0300

    ajustes em diretores

--- migrations/20181213212607_create_movies_table.js
+++ migrations/20181213212607_create_movies_table.js
@@ -1,15 +1,16 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('movies', table => {
-        table.increments('id').primary()
-        table.string('name', 80).notNullable()
-        table.string('description', 300).notNullable()
-        table.string('year', 4).notNullable()
-        table.integer('rating').unsigned().notNullable()
-        table.integer('genre_id').unsigned().notNullable()
-        table.foreign('genre_id').references('genres.id')
-    })
+  return knex.schema.createTable('movies', table => {
+      table.increments('id').primary()
+      table.string('name', 80).notNullable()
+      table.string('description', 300).notNullable()
+      table.string('year', 4).notNullable()
+      table.integer('director_id')
+      table.integer('rating').unsigned().notNullable()
+      table.integer('genre_id').unsigned().notNullable()
+      table.foreign('genre_id').references('genres.id')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('movies')
+return knex.schema.dropTable('movies')
 };
